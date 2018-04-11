e25e8a205c90faf163d908d5e2575c903869a965
Paul J. Durack <durack1@llnl.gov>
2018-04-11 11:01:29 -0700

    Add CMIPLib.py placeholder

---
+++ lib/CMIPLib.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Documentation for CMIPLib:
+-----
+Created on Wed Apr 11 10:59:24 2018
+
+Paul J. Durack and Stephen Po-Chedley 11th April 2018
+
+|  PJD 11 Apr 2018  - Initialized repository
+
+@author: durack1
+"""
+
