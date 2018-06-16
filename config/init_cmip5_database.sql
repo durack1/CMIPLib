3a1fff2da615479e2f1f994425f9da762a659843
Stephen Po-Chedley <pochedley@gmail.com>
2018-06-16 16:43:51 -0700

    Updated library to improve speed.

--- config/init_cmip5_database.sql
+++ config/init_cmip5_database.sql
@@ -1,13 +1,14 @@
 # to be run to initialize database if it doesn't exist
-# run with mysql -u root -p < init_database.sql
+# run with mysql -u root -p < init_cmip5_database.sql
 
 CREATE DATABASE cmip5;
 
 use cmip5;
 
 CREATE TABLE paths (pid int NOT NULL AUTO_INCREMENT, path varchar(255), product varchar(255), institute varchar(255), model varchar(255), experiment
-varchar(255), tfreq varchar(255), cmipTable varchar(255), realization varchar(255), version varchar(255), variable
-varchar(255), created DATETIME, modified DATETIME, accessed DATETIME, xmlFile varchar(255), xmlwritedate DATETIME, cdscanerror VARCHAR(255), PRIMARY KEY(pid));
+varchar(255), tfreq varchar(255), realm varchar(255), cmipTable varchar(255), realization varchar(255), version varchar(255), variable
+varchar(255), created DATETIME, modified DATETIME, accessed DATETIME, xmlFile varchar(255), xmlwritedatetime DATETIME, cdscanerror VARCHAR(255),
+retired TINYINT, retire_datetime DATETIME, PRIMARY KEY(pid));
 
 -- example user + permissions
 -- CREATE USER 'newuser'@'localhost' IDENTIFIED BY 'password';
